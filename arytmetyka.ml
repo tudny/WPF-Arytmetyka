da1f367f774f5bc54200e4340ab83c9d84f8db9c
Tudny <aleksanderwt@gmail.com>
2020-11-08 20:36:01 +0100

    First

---
+++ arytmetyka.ml
@@ -0,0 +1,18 @@
+
+(* typ 'wartosc' opisana w treści zadania *)
+(* zawiera dwa konstruktory *)
+(* Przedzial (a, b) - przechowuje informacje o zbiorze [a, b] *)
+(* Dopelnienie (x, y) - przechowuje informacje o zbiorze [-inf, x] u [y, inf] *)
+
+type wartosc = Przedzial of float * float |
+			   Dopelnienie of float * float;;
+			   
+(* ****************** *)
+(*    KONSTRUKTORY    *)
+(* ****************** *)
+
+let wartosc_dokladnosc x p =
+	let dokladnosc = abs_float (x *. p /. 100.) in 
+	Przedzial(x -. dokladnosc, x +. dokladnosc);;
+
+

2afa51de5a0acc2d226b6f52eb6a96bfa8207a85
Tudny <aleksanderwt@gmail.com>
2020-11-08 20:41:53 +0100

    Second

--- arytmetyka.ml
+++ arytmetyka.ml
@@ -5,14 +5,21 @@
 (* Dopelnienie (x, y) - przechowuje informacje o zbiorze [-inf, x] u [y, inf] *)
 
 type wartosc = Przedzial of float * float |
-			   Dopelnienie of float * float;;
+               Dopelnienie of float * float;;
 			   
 (* ****************** *)
 (*    KONSTRUKTORY    *)
 (* ****************** *)
 
 let wartosc_dokladnosc x p =
-	let dokladnosc = abs_float (x *. p /. 100.) in 
-	Przedzial(x -. dokladnosc, x +. dokladnosc);;
+    let dokladnosc = abs_float (x *. p /. 100.) in 
+    Przedzial(x -. dokladnosc, x +. dokladnosc);;
+
+let wartosc_od_do x y =
+    Przedzial(x, y);;
+
+let wartosc_dokladna x = 
+    wartosc_od_do x x;;
+
 
 
